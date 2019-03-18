29fd53812762f3b7f928fe8e7d7bed4b3bc719ee
Neeraj Jain <neeraj_jain@apple.com>
2019-03-18 14:23:27 +0530

    Added Redis DataStructures examples

--- src/main/java/com/neeraj/redis/RedisWithLettuce.java
+++ src/main/java/com/neeraj/redis/RedisWithLettuce.java
@@ -1,10 +1,17 @@
 package com.neeraj.redis;
 
 import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisFuture;
 import com.lambdaworks.redis.RedisURI;
+import com.lambdaworks.redis.TransactionResult;
 import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.api.async.RedisAsyncCommands;
 import com.lambdaworks.redis.api.sync.RedisCommands;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Class to experiment with Redis Client {@link com.lambdaworks.redis.RedisClient}
@@ -18,12 +25,14 @@ public class RedisWithLettuce {
 
     RedisClient redisClient;
     StatefulRedisConnection<String, String> statefulRedisConnection;
-    RedisCommands<String, String> commands;
+    RedisCommands<String, String> syncCommands;
+    RedisAsyncCommands<String, String> asyncCommands;
 
     public void connectToRedis() {
         redisClient = RedisClient.create(RedisURI.create("redis://localhost:6379"));
         statefulRedisConnection = redisClient.connect();
-        commands = statefulRedisConnection.sync();
+        syncCommands = statefulRedisConnection.sync();
+        asyncCommands = statefulRedisConnection.async();
         log.info("Connected to Redis using SSL");
     }
 
@@ -33,10 +42,64 @@ public class RedisWithLettuce {
     }
 
     public void writeDateToRedis(String key, String value) {
-        commands.set(key, value);
+        syncCommands.set(key, value);
     }
 
     public String getDataFromRedis(String key) {
-        return commands.get(key);
+        return syncCommands.get(key);
     }
+
+    public void writeDataToRedisHashSet(String key, String field, String value) {
+        syncCommands.hset(key, field, value);
+    }
+
+    public Map<String, String> getValuesFromHashSet(String key) {
+        return syncCommands.hgetall(key);
+    }
+
+    public RedisFuture<String> getDataFromRedisUsingAsyncCommands(String key) {
+        return asyncCommands.get(key);
+    }
+
+    // Redis Data Structures
+    // List
+    public void writeDataToRedisList(String key, String... values) {
+        asyncCommands.lpush(key, values);
+    }
+
+    public void deleteKey(String key) {
+        syncCommands.del(key);
+    }
+
+    public String getDataFromListInAscOrder(String key) {
+        return syncCommands.lpop(key);
+    }
+
+    public String getDataFromListInDescOrder(String key) {
+        return syncCommands.rpop(key);
+    }
+
+    // Sets
+    public void writeDataToRedisSetDataStructure(String key, String... values) {
+        asyncCommands.sadd(key, values);
+    }
+
+    public Set<String> getDataFromSet(String key) {
+        return syncCommands.smembers(key);
+    }
+
+    // Transactions in Redis
+    public TransactionResult transactionsInRedis() throws ExecutionException, InterruptedException {
+        asyncCommands.multi();
+
+        RedisFuture<String> result1 = asyncCommands.set("key1", "value1");
+        RedisFuture<String> result2 = asyncCommands.set("key2", "value2");
+        RedisFuture<String> result3 = asyncCommands.set("key3", "value3");
+
+        RedisFuture<TransactionResult> execResult = asyncCommands.exec();
+
+        // Here transaction is completed
+        return execResult.get();
+    }
+
 }
--- src/test/java/com/neeraj/redis/RedisWithLettuceTest.java
+++ src/test/java/com/neeraj/redis/RedisWithLettuceTest.java
@@ -1,7 +1,14 @@
 package com.neeraj.redis;
 
+import com.lambdaworks.redis.RedisFuture;
+import com.lambdaworks.redis.TransactionResult;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.*;
 
@@ -10,33 +17,82 @@ import static org.junit.Assert.*;
  * Copyright (c) 2019, RedisPlayground.
  * All rights reserved.
  */
+@Slf4j
 public class RedisWithLettuceTest {
     RedisWithLettuce redisWithLettuce;
 
     @Before
     public void init() {
         redisWithLettuce = new RedisWithLettuce();
+        redisWithLettuce.connectToRedis();
+    }
+
+    @After
+    public void cleanup() {
+        redisWithLettuce.closeAndShutdownRedisConnection();
     }
 
     @Test
     public void testIfConnectionToRedisIsSuccessful() {
-        redisWithLettuce.connectToRedis();
         assertTrue("Connection to Redis is successful", redisWithLettuce.statefulRedisConnection.isOpen());
     }
 
     @Test
     public void whenCloseAndShutdownRedisConnectionIsCalled_ItDisconnectsFromRedis() {
-        redisWithLettuce.connectToRedis();
         assertTrue("Connection to Redis is successful", redisWithLettuce.statefulRedisConnection.isOpen());
-
         redisWithLettuce.closeAndShutdownRedisConnection();
         assertFalse("Connection to Redis is closed", redisWithLettuce.statefulRedisConnection.isOpen());
     }
 
     @Test
     public void testOperationsInRedis() {
-        redisWithLettuce.connectToRedis();
         redisWithLettuce.writeDateToRedis("application.name", "RedisPlayground");
-        assertEquals(redisWithLettuce.getDataFromRedis("application.name"), "RedisPlayground");
+        assertEquals("RedisPlayground", redisWithLettuce.getDataFromRedis("application.name"));
     }
+
+    @Test
+    public void testWriteAndGetDataFromRedisHashSet() {
+        redisWithLettuce.writeDataToRedisHashSet("employee", "firstName", "Neeraj");
+        redisWithLettuce.writeDataToRedisHashSet("employee", "lastName", "Jain");
+        redisWithLettuce.writeDataToRedisHashSet("employee", "employeeId", "56421");
+        redisWithLettuce.writeDataToRedisHashSet("employee", "employer", "Apple");
+
+        Map<String, String> values = redisWithLettuce.getValuesFromHashSet("employee");
+
+        assertEquals(4, values.size());
+    }
+
+    @Test
+    public void getDataFromRedisUsingAsyncCommands() throws ExecutionException, InterruptedException {
+        redisWithLettuce.writeDateToRedis("testing:async", "Using StateFulRedis Connection's async command");
+        RedisFuture<String> future = redisWithLettuce.getDataFromRedisUsingAsyncCommands("testing:async");
+
+        String value = future.get();
+        assertNotNull(value);
+        assertEquals("Using StateFulRedis Connection's async command", value);
+    }
+
+    @Test
+    public void testListDataStructureWithRedis() {
+        redisWithLettuce.writeDataToRedisList("tasks", "Review PR", "Increase Code-Coverage", "Integrate Sonar");
+
+        assertEquals("Integrate Sonar", redisWithLettuce.getDataFromListInAscOrder("tasks"));
+        assertEquals("Review PR", redisWithLettuce.getDataFromListInDescOrder("tasks"));
+
+        redisWithLettuce.deleteKey("tasks");
+    }
+
+    @Test
+    public void testSetDataStructureWithRedis() {
+        redisWithLettuce.writeDataToRedisSetDataStructure("animals", "cat", "dog", "dog");
+        assertEquals(2, redisWithLettuce.getDataFromSet("animals").size());
+    }
+
+    @Test
+    public void testTransactionsInRedis() throws ExecutionException, InterruptedException {
+        TransactionResult transactionResult = redisWithLettuce.transactionsInRedis();
+        assertNotNull(transactionResult);
+        assertEquals(3, transactionResult.size());
+    }
+
 }
